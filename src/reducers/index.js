98ba96a851ad4a7dd4fc2fc0bb9d3c263c625ef8
Sergii <sergei.pryadko@genowavebart.com>
2018-02-17 15:22:48 +0300

    [add] redux page

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import users from './users';
+import userFriends from './userFriends';
+
+
+
+export default combineReducers({
+    users,
+    userFriends,
+});
